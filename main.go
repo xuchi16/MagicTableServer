aa6be6e8ffa4eac85e4099b63e5c669b3cb738d8
Lukathis <xuchi16@126.com>
2024-08-28 12:02:31 +0800

    revert: port 80

--- main.go
+++ main.go
@@ -13,5 +13,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows g"localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
